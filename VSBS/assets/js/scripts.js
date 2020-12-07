fb84bc9fddc00778cba7e9d64839a048b3a0b8f2
VSuc <60236074+VSuc@users.noreply.github.com>
2020-12-07 14:59:18 +0100

    del

--- VSBS/assets/js/scripts.js
+++ VSBS/assets/js/scripts.js
@@ -5,6 +5,32 @@ $("#header-button").click(function() {
 });
 
 
+function countup(){
+        var now = new Date();
+        var eventDate = new Date(1992, 5, 22);
+
+        var currentTiime = now.getTime();
+        var eventTime = eventDate.getTime();
+
+        var remTime =  currentTiime - eventTime;
+        var days = Math.floor(remTime/(24*3600*1000));
+        var months = Math.floor(days/30);
+        var years = Math.floor(months/12);
+        
+        months %= 12;
+        days %=30;
+
+
+        document.getElementById("days").textContent = days;
+        document.getElementById("months").textContent = months;
+        document.getElementById("years").textContent = years;
+
+        setTimeout(countup, 1000);
+    }
+
+    countup();
+
+
 $(function(){
     
     var latestImgArray=new Array();
